1f473f28e17df32fd3c9dae332dade30c1ed07cf
Elliot Kim <elliotkim916@gmail.com>
2020-04-16 16:59:51 -0700

    Able to show live location updates

--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -2,6 +2,8 @@ import createDataContext from './createDataContext';
 
 const locationReducer = (state, action) => {
   switch(action.type) {
+    case 'add_current_location':
+      return { ...state, currentLocation: action.payload };
     default:
       return state;
   }
@@ -11,10 +13,12 @@ const startRecording = dispatch => () => {};
 
 const stopRecording = dispatch => () => {};
 
-const addLocation = dispatch => () => {};
+const addLocation = dispatch => (location) => {
+  dispatch({ type: 'add_current_location', payload: location });
+};
 
 export const { Context, Provider } = createDataContext(
   locationReducer, 
-  { startRecording, stopRecording, addLocation}, 
+  { startRecording, stopRecording, addLocation }, 
   { recording: false, locations: [], currentLocation: null }
 );
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,12 +1,14 @@
 import '../_mockLocation';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
 import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
 import Map from '../components/Map';
+import { Context as LocationContext } from '../context/LocationContext';
 
 const TrackCreateScreen = () => {
+  const { addLocation } = useContext(LocationContext);
   const [error, setError] = useState(null);
 
   const startWatching = async () => {
@@ -17,7 +19,7 @@ const TrackCreateScreen = () => {
         timeInterval: 1000, // update once every second
         distanceInterval: 10 // update once every 10 meters
       }, (location) => {
-        console.log(location);
+        addLocation(location);
       });
     } catch(e) {
       setError(e);
@@ -31,7 +33,7 @@ const TrackCreateScreen = () => {
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
       <Text h2>Create a Track</Text>
-      <Map />
+      <Map></Map>
       {error ? <Text>Please enable location services</Text> : null}
     </SafeAreaView>
   );
