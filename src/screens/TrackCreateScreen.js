f3fc61c3bd511bae50ce2058af57b247de17cfaa
Elliot Kim <elliotkim916@gmail.com>
2020-04-15 19:21:59 -0700

    Added MapView on TrackCreateScreen

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { Text } from 'react-native-elements';
+import { SafeAreaView } from 'react-navigation';
+import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
   return (
-    <Text style={{ fontSize: 48 }}>Track Create Screen</Text>
+    <SafeAreaView forceInset={{ top: 'always' }}>
+      <Text h2>Create a Track</Text>
+      <Map />
+    </SafeAreaView>
   );
 };
 
